80eda91864c72b3615299427c7642aa9dc609282
Ricardo <ricardobassanesi@gmail.com>
2024-11-09 13:36:07 -0300

    enunciado

---
+++ projeto_1.py
@@ -0,0 +1,30 @@
+'''
+Enunciado da fase 1
+
+Implemente um programa que leia, valide e analise dados informados pelo usuário. Os dados são meteorológicos e referem-se aos dados de 2021 (de janeiro a dezembro) registrados em uma cidade. 
+
+ 
+
+Toda entrada de dados deve ser validada. No caso de valor inválido, informe ao usuário o erro e permita que ele redigite o dado.
+
+ 
+
+Seu programa deve coletar os seguintes dados:
+
+
+• Mês: use valor numérico de 1 a 12 (janeiro a dezembro) para se referir aos meses do ano.   
+
+Para cada mês do ano, informe:  
+• Temperatura máxima do mês: devem estar em Celsius, garanta que estejam dentro de um intervalo válido para temperaturas, tal como: -60 graus à +50 graus.   
+
+A seguir, seu programa deve calcular e exibir:  
+• Temperatura média máxima anual: exibe a média das temperaturas máximas informadas.
+• Quantidade de meses escaldantes: quantidade de meses com temperaturas máximas acima de 33 graus Celsius.
+• Mês mais escaldante do ano: mês que registrou a maior temperatura máxima dentre todos os informados. O mês deve ser escrito na tela por extenso (janeiro a dezembro). 
+• Mês menos quente do ano: mês que registrou a menor temperatura máxima dentre todos os informados. O mês deve ser escrito na tela por extenso (janeiro a dezembro).  
+
+Observações gerais:
+
+Use repetição para ler os dados e calcular as estatísticas solicitadas.
+Não é necessário manter os dados lidos na memória após término do programa.    
+'''
